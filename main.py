7e478a5c3581f947a87eb0a753f420832af1bad6
shooby <shoubaneh@gmail.com>
2019-06-22 20:29:03 -0700

    nemidunam, psf doros nis

--- main.py
+++ main.py
@@ -21,25 +21,25 @@ psf = pyfits.getdata('psf_h.fits')
 psf = np.asarray(psf[62:-62,62:-62],dtype='d')
 psfh = np.repeat(psf[:,:, np.newaxis], 1, axis=2)
 psfh = np.repeat(psfh[:,:,:,np.newaxis],1,axis = 3)
-kernel = torch.Tensor(psfh)
-kernel = kernel.permute(2,3,0,1)
-ker = kernel.cuda()
+ker = torch.Tensor(psfh)
+ker = ker.permute(2,3,0,1)
+kernel = ker.cuda()
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
 parser.add_argument('--dataroot', default='gals/', help='path to dataset')
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
-parser.add_argument('--nz', type=int, default=25, help='size of the latent z vector')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=2, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=100, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
-parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
@@ -121,8 +121,12 @@ class Generator(nn.Module):
     def forward(self, input):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            output = F.conv2d(output, kernel,padding=int(((kernel.shape[3])-1)/2))
+
         else:
             output = self.main(input)
+            ougpug = F.conv2d(output, kernel,padding=int(((kernel.shape[3])-1)/2))
+
         return output
 
 
@@ -204,7 +208,7 @@ for epoch in range(opt.niter):
         fake = netG(noise)
                 
         ####################PSF convolution added by Shooby##########################    
-        fake = F.conv2d(fake, kernel,padding=int(((kernel.shape[3])-1)/2))
+        #fake = F.conv2d(fake, kernel,padding=int(((kernel.shape[3])-1)/2))
         #############################################################################
         
         label.fill_(fake_label)
@@ -239,5 +243,5 @@ for epoch in range(opt.niter):
                     normalize=True)
 
     # do checkpointing
-    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
-    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+    torch.save(netG.state_dict(), '%s/outputs/netG_epoch_%d.pth' % (opt.outf, epoch))
+    torch.save(netD.state_dict(), '%s/outputs/netD_epoch_%d.pth' % (opt.outf, epoch))
