77b09241c30ff0034e58ba83829f80cd94af65dc
xoubish <shoubaneh@gmail.com>
2019-06-22 22:51:50 -0700

    moved tanh to after convolution

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ parser.add_argument('--imageSize', type=int, default=64, help='the height / widt
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=100, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=50, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -114,7 +114,7 @@ class Generator(nn.Module):
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
-            nn.Tanh()
+            #nn.Tanh()
             # state size. (nc) x 64 x 64
         )
 
@@ -122,10 +122,14 @@ class Generator(nn.Module):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
             output = F.conv2d(output, kernel,padding=int(((kernel.shape[3])-1)/2))
+            output = output.tanh()
+
 
         else:
             output = self.main(input)
-            ougpug = F.conv2d(output, kernel,padding=int(((kernel.shape[3])-1)/2))
+            output = F.conv2d(output, kernel,padding=int(((kernel.shape[3])-1)/2))
+            output = output.tanh()
+
 
         return output
 
