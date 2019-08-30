427cbe3e82fde9edcb881be18ce0f0d8f0746c35
xoubish <shoubaneh@gmail.com>
2019-08-30 12:11:14 -0700

    added arcsinh to all data for normalization, added tensorboard writer

--- main_train.py
+++ main_train.py
@@ -17,8 +17,7 @@ import torch.nn.functional as F
 import astropy.io.fits as pyfits
 import numpy as np
 
-
-
+from torch.utils.tensorboard import SummaryWriter
 
 from skimage.transform import downscale_local_mean
 from scipy.ndimage import zoom
@@ -39,14 +38,14 @@ kernel = kernel.cuda()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', default='gals_optim/', help='path to dataset')
+parser.add_argument('--dataroot', default='gals_galsim/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=10, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -193,6 +192,8 @@ fake_label = 0
 optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerS = optim.Adam(netS.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
+writer = SummaryWriter()
+
 for epoch in range(opt.niter):
     for i, data in enumerate(dataloader, 0):
         ############################
@@ -234,6 +235,10 @@ for epoch in range(opt.niter):
         errG.backward()
         D_G_z2 = output.mean().item()
         optimizerS.step()
+        
+        writer.add_scalar('Generator/train', errG.item(), niter)
+        writer.add_scalar('Discriminator/train', errD.item(), niter)
+
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
                  errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
@@ -248,3 +253,4 @@ for epoch in range(opt.niter):
     # do checkpointing
     torch.save(netS.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+    writer.close()
