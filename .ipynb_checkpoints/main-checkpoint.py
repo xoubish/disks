f5ec330b1c8df60801789eb2846e13b42a00db3e
xoubish <shoubaneh@gmail.com>
2019-08-29 18:12:55 -0700

    typo

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -233,7 +233,7 @@ for epoch in range(opt.niter):
         D_G_z2 = output.mean().item()
         optimizerS.step()
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-              % (epoch, niter, i, len(dataloader),
+              % (epoch, opt.niter, i, len(dataloader),
                  errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         if i % 100 == 0:
             vutils.save_image(real_cpu,
