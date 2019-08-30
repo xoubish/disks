ca8b3adaff77d537f81d991112c401be75e52ae1
xoubish <shoubaneh@gmail.com>
2019-08-29 20:39:04 -0700

    maybe done with training data

---
+++ ghadimi/.ipynb_checkpoints/image_gan_test-checkpoint.py
@@ -0,0 +1,168 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+import torch.nn.functional as F
+from photutils import create_matching_kernel
+
+import astropy.io.fits as pyfits
+import numpy as np
+
+
+###### making the CUDA Kernel transfering HST to Subaru HSC###
+from skimage.transform import downscale_local_mean
+from scipy.ndimage import zoom
+
+psf = pyfits.getdata('psf_i.fits')
+psf = downscale_local_mean(psf,(3,3))
+psf = psf[7:-8,7:-8]#[22:-22,22:-22]
+
+psf_hsc = pyfits.getdata('psf-calexp-s16a_wide-HSC-I-15827-7,2-236.00000-42.00000.fits')
+psf_hsc = psf_hsc[0:41,1:42]
+
+kern = create_matching_kernel(psf,psf_hsc)
+psfh = np.repeat(kern[:,:, np.newaxis], 1, axis=2)
+psfh = np.repeat(psfh[:,:,:,np.newaxis],1,axis = 3)
+kernel = torch.Tensor(psfh)
+kernel = kernel.permute(2,3,0,1)
+kernel =  kernel.float()
+
+
+##### Defenitions and GAN default params
+dataroot='gals/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
+parser.add_argument('--dataroot', default='gals/', help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=1000, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=200, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--output', default='GAN_perfomance.png', help='ha')
+
+
+opt = parser.parse_args()
+print(opt)
+
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+nc = 1
+
+device = torch.device("cuda:0" if opt.cuda else "cpu")
+ngpu = int(opt.ngpu)
+nz = int(opt.nz)
+ngf = int(opt.ngf)
+ndf = int(opt.ndf)
+
+
+# custom weights initialization called on netS and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class Shoobygen(nn.Module):
+
+    def __init__(self,ngpu):
+        super(Shoobygen, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            
+            nn.ConvTranspose2d( 1, ngf * 8, 4, 1, 2, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 2, 2, 2, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+  
+            nn.ConvTranspose2d(ngf*4, nc, 2, 2, 4, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+
+        else:
+            output = self.main(input)
+
+        return output
+
+
+netS = Shoobygen(ngpu).to(device)
+
+
+
+### read in an image with HST resoluion and downgrade (resample, PSF convolve, add noise) to HSC 
+dataset = dset.MNIST(root=dataroot, download=True, transform=transforms.Compose([transforms.Resize(opt.imageSize),transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,)),]))
+nc=1
+
+assert dataset
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=int(opt.workers))
+
+inputs, classes = next(iter(dataloader))  
+real_cpu = inputs.to(device)
+ajab = real_cpu.detach()
+
+
+kernel = kernel.to(device)
+im = real_cpu+0.25*torch.rand_like(real_cpu)
+downsampled = F.upsample(im,scale_factor=1/3,mode='bilinear')
+img = F.conv2d(downsampled, kernel,padding=int(((kernel.shape[3])-1)/2))
+img = img[:,:,:,:]
+
+### Use GAN to improve resolution
+netS.load_state_dict(torch.load('netG_epoch_490.pth',map_location='cpu'))
+fake = netS(img)
+fd = fake.detach()
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12,4))
+plt.subplot(1,3,1)
+plt.imshow(ajab[0,0,:,:],origin='lower')
+plt.text(1,1,'Original',color='y')
+plt.subplot(1,3,2)
+plt.imshow(img[0,0,:,:],origin='lower')
+plt.text(1,1,'downgrade',color='y')
+plt.subplot(1,3,3)
+plt.imshow(fd[0,0,:,:],origin='lower')
+plt.text(1,1,'recovered',color='y')
+plt.savefig(opt.output)
