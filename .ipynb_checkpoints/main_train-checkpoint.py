20ea1ea69097a31d0e18e9972130455017e81c16
xoubish <shoubaneh@gmail.com>
2019-08-30 13:13:57 -0700

    lets see

--- .ipynb_checkpoints/main_train-checkpoint.py
+++ .ipynb_checkpoints/main_train-checkpoint.py
@@ -45,7 +45,7 @@ parser.add_argument('--imageSize', type=int, default=64, help='the height / widt
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=10, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=1000, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -236,20 +236,17 @@ for epoch in range(opt.niter):
         D_G_z2 = output.mean().item()
         optimizerS.step()
         
-        writer.add_scalar('Generator/train', errG.item(), niter)
-        writer.add_scalar('Discriminator/train', errD.item(), niter)
+        writer.add_scalar('Generator/train', errG.item(), epoch)
+        writer.add_scalar('Discriminator/train', errD.item(), epoch)
 
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
                  errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         if i % 100 == 0:
-            vutils.save_image(real_cpu,
-                    '%s/real_samples.png' % opt.outf,
-                    normalize=True)
+            vutils.save_image(real_cpu,'%s/real_samples.png' % opt.outf,normalize=True)
             fake = netS(img)
-            vutils.save_image(fake.detach(),
-                    '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
-                    normalize=True)
+            vutils.save_image(fake.detach(),'%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),normalize=True)
+            
     # do checkpointing
     torch.save(netS.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
