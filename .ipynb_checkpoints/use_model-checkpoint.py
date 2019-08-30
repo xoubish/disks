7b991fc3679484f713f56b3045d926981ffcfa4e
xoubish <shoubaneh@gmail.com>
2019-08-29 19:25:29 -0700

    trying randomly

--- .ipynb_checkpoints/use_model-checkpoint.py
+++ .ipynb_checkpoints/use_model-checkpoint.py
@@ -11,31 +11,38 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-
 import torch.nn.functional as F
-
+from photutils import create_matching_kernel
+from skimage.transform import downscale_local_mean
+from scipy.ndimage import zoom
+import matplotlib.pyplot as plt
 import astropy.io.fits as pyfits
 import numpy as np
 
-psf = pyfits.getdata('psf_h.fits')
-psf = np.asarray(psf[62:-62,62:-62],dtype='d')
-psfh = np.repeat(psf[:,:, np.newaxis], 1, axis=2)
+psf = pyfits.getdata('psf_i.fits')
+psf = downscale_local_mean(psf,(3,3))
+psf = psf[7:-8,7:-8]#[22:-22,22:-22]
+psf_hsc = pyfits.getdata('psf-calexp-s16a_wide-HSC-I-15827-7,2-236.00000-42.00000.fits')
+psf_hsc = psf_hsc[0:41,1:42]
+kern = create_matching_kernel(psf,psf_hsc)
+psfh = np.repeat(kern[:,:, np.newaxis], 1, axis=2)
 psfh = np.repeat(psfh[:,:,:,np.newaxis],1,axis = 3)
-ker = torch.Tensor(psfh)
-ker = ker.permute(2,3,0,1)
-kernel = ker.cuda()
+kernel = torch.Tensor(psfh)
+kernel = kernel.permute(2,3,0,1)
+kernel =  kernel.float()
+#kernel = kernel.cuda()
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', default='gals/', help='path to dataset')
+parser.add_argument('--dataroot', default='gals_optim/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=1000, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=200, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -83,51 +90,63 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 
-class Generator2(nn.Module):
-    def __init__(self, ngpu):
-        super(Generator2, self).__init__()
+class Shoobygen(nn.Module):
+
+    def __init__(self,ngpu):
+        super(Shoobygen, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            
+            nn.ConvTranspose2d( 1, ngf * 8, 4, 1, 2, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 2, 2, 2, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+  
+            nn.ConvTranspose2d(ngf*4, nc, 2, 2, 4, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
         )
 
     def forward(self, input):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            output1 = output[:,:,:,:]
+
         else:
             output = self.main(input)
-        return output
+            output1 = output[:,:,:,:]
 
-netG2 = Generator2(ngpu).to(device)
+        return output1
 
-fixed_noise = torch.randn(opt.batchSize, nz, 1, 1, device=device)
+netS = Shoobygen(ngpu).to(device)
+netS.load_state_dict(torch.load('keep_33.pth'))
 
-netG2.load_state_dict(torch.load('outputs/netG_epoch_199.pth'))
-fake = netG2(fixed_noise)
+dataloader = torch.utils.data.DataLoader('gals_optim/MNIST/processed/test.pt', batch_size=1, shuffle=True, num_workers=1)
+
+print(dataloader)
+inputs = dataloader
+stop
+real_cpu = inputs.to(device)
+ajab = real_cpu.detach()
+
+im = real_cpu+0.25*torch.rand_like(real_cpu)
+downsampled = F.upsample(im,scale_factor=1/3,mode='bilinear')
+img = F.conv2d(downsampled, kernel,padding=int(((kernel.shape[3])-1)/2))
+img = img[:,:,:,:]
+
+fake = netS(img)
 fd = fake.detach()
-ajab = fd.cpu()
-ajab = ajab[0,0,:,:].data.numpy()
-pyfits.writeto('test.fits',ajab,clobber=True)
-vutils.save_image(fd,'test.png', normalize=True)
-output = F.conv2d(fake, kernel,padding=int(((kernel.shape[3])-1)/2))
-output = output.tanh()
-vutils.save_image(output.detach(),'test2.png', normalize=True)
+
+
+plt.figure(figsize=(12,4))
+plt.subplot(1,3,1)
+plt.imshow(ajab[0,0,:,:],origin='lower')
+plt.text(1,1,'Original (Galsim)',color='y')
+plt.subplot(1,3,2)
+plt.imshow(img[0,0,:,:],origin='lower')
+plt.text(1,1,'downgrade',color='y')
+plt.subplot(1,3,3)
+plt.imshow(fd[0,0,:,:],origin='lower')
+plt.text(1,1,'recovered',color='y')
