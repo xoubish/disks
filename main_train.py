e8c56be7c23acd8def25613c3f0fc7550f3c85cd
shooby <shoubaneh@gmail.com>
2019-09-05 12:44:37 -0700

    couple of runs

--- main_train.py
+++ main_train.py
@@ -38,7 +38,7 @@ kernel = kernel.cuda()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', default='gals_candels/', help='path to dataset')
+parser.add_argument('--dataroot', default='gals_optim/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
@@ -46,8 +46,8 @@ parser.add_argument('--nz', type=int, default=100, help='size of the latent z ve
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--lr', type=float, default=0.002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.7, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
 parser.add_argument('--netS', default='', help="path to netS (to continue training)")
@@ -77,9 +77,7 @@ if torch.cuda.is_available() and not opt.cuda:
 
     
 dataset = dset.MNIST(root=opt.dataroot, download=True,
-                     transform=transforms.Compose([transforms.Resize(opt.imageSize),transforms.ToTensor(),]))
-
-#transforms.Normalize((0.5,), (0.5,))
+                     transform=transforms.Compose([transforms.Resize(opt.imageSize),transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]))
 nc=1
 
 assert dataset
