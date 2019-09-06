b1e42ea7f20ce1ab88e7f58cf523941b8d8ca17b
xoubish <shoubaneh@gmail.com>
2019-09-05 20:50:46 -0700

    added more tensorboard plot codes

--- .ipynb_checkpoints/main_train-checkpoint.py
+++ .ipynb_checkpoints/main_train-checkpoint.py
@@ -13,6 +13,8 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from photutils import create_matching_kernel
 import torch.nn.functional as F
+import torchvision
+
 
 import astropy.io.fits as pyfits
 import numpy as np
@@ -38,14 +40,14 @@ kernel = kernel.cuda()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', default='gals_candels/', help='path to dataset')
+parser.add_argument('--dataroot', default='gals_optim/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=1000, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -77,9 +79,7 @@ if torch.cuda.is_available() and not opt.cuda:
 
     
 dataset = dset.MNIST(root=opt.dataroot, download=True,
-                     transform=transforms.Compose([transforms.Resize(opt.imageSize),transforms.ToTensor(),]))
-
-#transforms.Normalize((0.5,), (0.5,))
+                     transform=transforms.Compose([transforms.Resize(opt.imageSize),transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]))
 nc=1
 
 assert dataset
@@ -236,8 +236,11 @@ for epoch in range(opt.niter):
         D_G_z2 = output.mean().item()
         optimizerS.step()
         
-        writer.add_scalar('Generator/train', errG.item(), epoch)
-        writer.add_scalar('Discriminator/train', errD.item(), epoch)
+        writer.add_scalar('Generator/Error', errG.item(), epoch)
+        writer.add_scalar('Discriminator/Error', errD.item(), epoch)
+        writer.add_scalar('Discriminator/mean_out_real', D_x, epoch)
+        writer.add_scalar('Discriminator/mean_out_fake', D_G_z1, epoch)
+
 
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
@@ -246,6 +249,10 @@ for epoch in range(opt.niter):
             vutils.save_image(real_cpu,'%s/real_samples.png' % opt.outf,normalize=True)
             fake = netS(img)
             vutils.save_image(fake.detach(),'%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),normalize=True)
+            
+            grid = torchvision.utils.make_grid(fake.detach())
+            writer.add_image('images',grid,0)
+
             
     # do checkpointing
     torch.save(netS.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
--- main_train.py
+++ main_train.py
@@ -13,6 +13,8 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from photutils import create_matching_kernel
 import torch.nn.functional as F
+import torchvision
+
 
 import astropy.io.fits as pyfits
 import numpy as np
@@ -45,9 +47,9 @@ parser.add_argument('--imageSize', type=int, default=64, help='the height / widt
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.002, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', type=float, default=0.7, help='beta1 for adam. default=0.5')
+parser.add_argument('--niter', type=int, default=1000, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
 parser.add_argument('--netS', default='', help="path to netS (to continue training)")
@@ -234,8 +236,11 @@ for epoch in range(opt.niter):
         D_G_z2 = output.mean().item()
         optimizerS.step()
         
-        writer.add_scalar('Generator/train', errG.item(), epoch)
-        writer.add_scalar('Discriminator/train', errD.item(), epoch)
+        writer.add_scalar('Generator/Error', errG.item(), epoch)
+        writer.add_scalar('Discriminator/Error', errD.item(), epoch)
+        writer.add_scalar('Discriminator/mean_out_real', D_x, epoch)
+        writer.add_scalar('Discriminator/mean_out_fake', D_G_z1, epoch)
+
 
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
@@ -244,6 +249,10 @@ for epoch in range(opt.niter):
             vutils.save_image(real_cpu,'%s/real_samples.png' % opt.outf,normalize=True)
             fake = netS(img)
             vutils.save_image(fake.detach(),'%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),normalize=True)
+            
+            grid = torchvision.utils.make_grid(fake.detach())
+            writer.add_image('images',grid,0)
+
             
     # do checkpointing
     torch.save(netS.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
