a275444d5005f20439fc2c29034ed37baf6e5d72
xoubish <shoubaneh@gmail.com>
2019-09-09 14:27:33 -0700

    nothing

--- .ipynb_checkpoints/main_train-checkpoint.py
+++ .ipynb_checkpoints/main_train-checkpoint.py
@@ -40,7 +40,7 @@ kernel = kernel.cuda()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', default='gals_optim/', help='path to dataset')
+parser.add_argument('--dataroot', default='gals_galsim/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
@@ -48,8 +48,8 @@ parser.add_argument('--nz', type=int, default=100, help='size of the latent z ve
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--niter', type=int, default=1000, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--lr', type=float, default=0.0001, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
 parser.add_argument('--netS', default='', help="path to netS (to continue training)")
@@ -251,7 +251,7 @@ for epoch in range(opt.niter):
             vutils.save_image(fake.detach(),'%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),normalize=True)
             
             grid = torchvision.utils.make_grid(fake.detach())
-            writer.add_image('images',grid,0)
+            writer.add_image('images',grid,i)
 
             
     # do checkpointing
