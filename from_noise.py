6a0e061f8dc91ceaffb60e9d0900ca0a3db8b81d
shooby <shoubaneh@gmail.com>
2019-10-22 11:52:34 -0700

    from noise GAN fixed

--- from_noise.py
+++ from_noise.py
@@ -38,7 +38,7 @@ parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, def
 parser.add_argument('--beta1', type=float, default=0.8, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=3, help='number of GPUs to use')
-parser.add_argument('--netS', default='', help="path to netS (to continue training)")
+parser.add_argument('--netG', default='', help="path to netS (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--outf', default='outputs/', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
@@ -69,6 +69,7 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+
 class Generator(nn.Module):
     def __init__(self, ngpu):
         super(Generator, self).__init__()
@@ -106,8 +107,8 @@ class Generator(nn.Module):
 
 netG = Generator(ngpu).to(device)
 netG.apply(weights_init)
-if netG != '':
-    netG.load_state_dict(torch.load(netG))
+if opt.netG != '':
+    netG.load_state_dict(torch.load(opt.netG))
 print(netG)
 
 class Discriminator(nn.Module):
@@ -146,21 +147,24 @@ class Discriminator(nn.Module):
 
 netD = Discriminator(ngpu).to(device)
 netD.apply(weights_init)
-if netD != '':
-    netD.load_state_dict(torch.load(netD))
+if opt.netD != '':
+    netD.load_state_dict(torch.load(opt.netD))
 print(netD)
 
 criterion = nn.BCELoss()
 
-fixed_noise = torch.randn(batchSize, nz, 1, 1, device=device)
+fixed_noise = torch.randn(opt.batchSize, nz, 1, 1, device=device)
 real_label = 1
 fake_label = 0
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=0.002, betas=(0.8, 0.999))
 
-for epoch in range(niter):
+
+writer = SummaryWriter()
+
+for epoch in range(opt.niter):
     for i, data in enumerate(dataloader, 0):
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
@@ -200,18 +204,24 @@ for epoch in range(niter):
         D_G_z2 = output.mean().item()
         optimizerG.step()
 
+
+        writer.add_scalar('Generator/Error', errG.item(), epoch)
+        writer.add_scalar('Discriminator/Error', errD.item(), epoch)
+        writer.add_scalar('Discriminator/mean_out_real', D_x, epoch)
+        writer.add_scalar('Discriminator/mean_out_fake', D_G_z1, epoch)
+
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-              % (epoch, niter, i, len(dataloader),
+              % (epoch, opt.niter, i, len(dataloader),
                  errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         if i % 100 == 0:
             vutils.save_image(real_cpu,
-                    '%s/real_samples.png' % outf,
+                    '%s/real_samples.png' % opt.outf,
                     normalize=True)
             fake = netG(fixed_noise)
             vutils.save_image(fake.detach(),
-                    '%s/fake_samples_epoch_%03d.png' % (outf, epoch),
+                    '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
                     normalize=True)
 
     # do checkpointing
-    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (outf, epoch))
-    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (outf, epoch))
+    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
+    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
