89dfb2eb03b585e63b515e2296860caa14a30cbd
emhuff <emhuff@gmail.com>
2020-10-21 21:42:02 -0700

    deblend_sim now runs, but gets fluxes wrong. Probably a problem in indexing the positions.

--- Blending/deblend_sim.py
+++ Blending/deblend_sim.py
@@ -1,6 +1,9 @@
 import numpy as np
 import galblend
 import matplotlib.pyplot as plt
+import ngmix
+import mof
+import ipdb
 
 
 class ObjectData():
@@ -18,25 +21,27 @@ class ObjectData():
 
         # Here let's just choose the closest object to the center.
         xcen,ycen = image_size/2.,image_size/2.
-        dist = np.sqrt((x_lores - xcen)**2 + (y_lores-ycen)**2)
+        dist = np.sqrt((np.array(x_lores) - xcen)**2 + (np.array(y_lores)-ycen)**2)
         self.lores_match_ind = np.argmin(dist)
         
         self.x_lores = x_lores[self.lores_match_ind]
         self.y_lores = y_lores[self.lores_match_ind]
 
-        self.nobj_true = len(x_true)
-        self.nobj_gan = len(x_gan)
-        self.nobj_lores = len(x_lores)
+        self.nobj_true = len(self.x_true)
+        self.nobj_gan = len(self.x_gan)
+        self.nobj_lores = 1
 
 
 class Simulation(object):
 
-    def __init__(self,number_of_images = 100, do_image_plots=False):
+    def __init__(self,number_of_images = 100, do_image_plots=False,max_tries=5,max_chi2perdof = 2.,flux_calibration = 5.07224239892):
         self.number_of_images = number_of_images
         self.do_image_plots = do_image_plots
         self.scale_hires = 1.0/3.0
-        scale.scale_lores = 1.0
-
+        self.scale_lores = 1.0
+        self.max_tries = max_tries # for fitting.
+        self.max_chi2 = max_chi2perdof
+        self.flux_calibration = flux_calibration
 
 
     def make_simulated_data(self):
@@ -59,6 +64,10 @@ class Simulation(object):
             xhi,yhi = data[0],data[1]
             xgan,ygan = data[-1][-1][0], data[-1][-1][1]
             xlo,ylo = data[-1][-2]
+            # Make sure that something was detected in every image.
+            if (len(xlo) == 0) or (len(xgan) != 2) or (len(xhi) != 2):
+                continue
+            
             thisData = ObjectData(x_true=xhi,y_true=yhi, x_gan=xgan, y_gan = ygan, x_lores = xlo, y_lores=ylo)
             self.object_data.append(thisData)
 
@@ -80,43 +89,41 @@ class Simulation(object):
                          ('lores_deblended_flux1',np.float),('lores_deblended_flux2',np.float),
                          ('lores_deblended_flux1_err',np.float),('lores_deblended_flux2_err',np.float),
                          ('lores_blended_flux',np.float),('lores_blended_flux_err',np.float)]
-        self.catalog = np.empty(self.number_of_images,dtype=catalog_dtype)
+        self.catalog = np.zeros(len(self.object_data),dtype=catalog_dtype)
         
         for i,datum in enumerate(self.object_data):
 
             # First, fit each hires image separately.
-            result1 = self._fit_one_obs(single_obs1[i],datum.x_true[0],datum.y_true[0])
-            result2 = self._fit_one_obs(single_obs1[2],datum.x_true[1],datum.y_true[1])
+            result1 = self._fit_one_obs(self.single_obs1[i],datum.x_true[0],datum.y_true[0])
+            result2 = self._fit_one_obs(self.single_obs2[i],datum.x_true[1],datum.y_true[1])
             # Package the results into convenient catalog format.
-            self.catalog[i]['input_flux1'] = result1[0]['flux']
-            self.catalog[i]['input_flux1_err'] = result1[0]['flux_err']
-            self.catalog[i]['input_flux2'] = result2[0]['flux']
-            self.catalog[i]['input_flux2_err'] = result2[0]['flux_err']
+            self.catalog[i]['input_flux1'] = result1[0]['flux']/self.flux_calibration
+            self.catalog[i]['input_flux1_err'] = result1[0]['flux_err']/self.flux_calibration
+            self.catalog[i]['input_flux2'] = result2[0]['flux']/self.flux_calibration
+            self.catalog[i]['input_flux2_err'] = result2[0]['flux_err']/self.flux_calibration
 
             # Then, deblend on lores.
-            deblend_result1,deblend_result2 = self._fit_one_obs( self.lores_obs,datum.x_gan,datum.y_gan )
+            deblend_result1,deblend_result2 = self._fit_one_obs( self.lores_obs[i],datum.x_gan,datum.y_gan )
             # Package the results into convenient catalog format.
-            self.catalog[i]['lores_deblended_flux1'] = deblend_result1[0]['flux']
-            self.catalog[i]['lores_deblended_flux1_err'] = deblend_result1[0]['flux_err']            
-            self.catalog[i]['lores_deblended_flux2'] = deblend_result2[0]['flux']
-            self.catalog[i]['lores_deblended_flux2'] = deblend_result2[0]['flux_err']
+            self.catalog[i]['lores_deblended_flux1'] = deblend_result1['flux']/self.flux_calibration
+            self.catalog[i]['lores_deblended_flux1_err'] = deblend_result1['flux_err']/self.flux_calibration
+            self.catalog[i]['lores_deblended_flux2'] = deblend_result2['flux']/self.flux_calibration
+            self.catalog[i]['lores_deblended_flux2'] = deblend_result2['flux_err']/self.flux_calibration
             
             # Finally, don't deblend.
-            blend_result = self._fit_one_obs(self.lores_obs, datum.x_lores,datum.y_lores)
-            self.catalog[i]['lores_blended_flux'] = blend_result['flux']
-            self.catalog[i]['lores_blended_flux_err'] = blend_result['flux_err']
+            blend_result = self._fit_one_obs(self.lores_obs[i], datum.x_lores,datum.y_lores)
+            self.catalog[i]['lores_blended_flux'] = blend_result[0]['flux']/self.flux_calibration
+            self.catalog[i]['lores_blended_flux_err'] = blend_result[0]['flux_err']/self.flux_calibration
             
                 
-    def _make_observation(self, image,psf,image_scale=1.0, psf_scale=1.0, image_noise = .055,psf_noise = 1e-3, scale = 1.0):
-        gal_image = get_image(objid)
-        psf_image = get_psf(objid)
+    def _make_observation(self, gal_image,psf_image,image_scale=1.0, psf_scale=1.0, image_noise = .055,psf_noise = 1e-3, scale = 1.0):
         weight_image = np.ones_like(gal_image) + 1./image_noise**2
         psf_weight_image = np.ones_like(psf_image) + 1./psf_noise**2
 
         jj_im = ngmix.jacobian.DiagonalJacobian(scale=image_scale,col=0,row=0)
         jj_psf = ngmix.jacobian.DiagonalJacobian(scale=psf_scale,col=(psf_image.shape[0]-1)/2.,row=(psf_image.shape[0]-1)/2.)
         psf_obs = ngmix.Observation(psf_image,weight=psf_weight_image,jacobian=jj_psf)
-        psf_gmix = fit_psf(psf_obs)
+        psf_gmix = self._fit_psf(psf_obs)
         psf_obs.set_gmix(psf_gmix)
         gal_obs = ngmix.Observation(gal_image, weight=weight_image,jacobian=jj_im, psf=psf_obs)
     
@@ -130,7 +137,7 @@ class Simulation(object):
         y_pos: list of y-positions for objects in image.
         '''
 
-        assert len(x_pos) == len(y_pos),"x and y positions must be the same length."
+        #assert len(x_pos) == len(y_pos),"x and y positions must be the same length."
         
         # prior on ellipticity.  The details don't matter, as long
         # as it regularizes the fit.  This one is from Bernstein & Armstrong 2014
@@ -198,16 +205,27 @@ class Simulation(object):
         result['fracDev_err'] = pars[5]
         result['flux_err'] = pars[6]
         return result
+
     
+    def _fit_psf(self,psfObs):
+        lm_pars = {'ftol': 1.0e-5,
+                    'xtol': 1.0e-5}
+        psf_boot = ngmix.bootstrap.PSFRunner(psfObs,'turb',1.0,lm_pars=lm_pars)
+        psf_boot.go()
+        gm_psf = psf_boot.fitter.get_gmix()
+        return gm_psf
     
 
-    def _fit_one_obs(self,obs,xpos,ypos,fitpars = {'ftol': 1.0e-5,'xtol': 1.0e-5}):
-        prior = self._get_priors(x_xpos=xpos, y_pos=ypos)
+    def _fit_one_obs(self,obs,xpos,ypos,fitpars = {'ftol': 1.0e-5,'xtol': 1.0e-5},max_tries = 5):
+        if type(xpos) is not type([]):
+            xpos = [xpos]
+            ypos = [ypos]
+        prior = self._get_priors(x_pos=xpos, y_pos=ypos)
         npars_per_object = 7 # Only for BDF fits
         n_object = len(xpos)
         acceptable_fit = False
         n_tries = 0
-        while (n_tries <= max_tries) & (not acceptable_fit):
+        while (n_tries <= self.max_tries) & (not acceptable_fit):
             guess = prior.sample()
             fitter = mof.moflib.MOF(obs,model='bdf',nobj=n_object,prior=prior)
             fitter.go(guess=guess)
@@ -217,7 +235,7 @@ class Simulation(object):
                 pars = fitter.get_result()['pars']
                 chi2 = result['chi2per']
                 print(f"{chi2}")
-                if chi2 < max_chi2:
+                if chi2 < self.max_chi2:
                     acceptable_fit = True
                     print ("good enough")
                 else:
@@ -242,3 +260,10 @@ class Simulation(object):
         self.fit_simulated_data()
 
     
+
+
+if __name__ == '__main__':
+    sim = Simulation(number_of_images=100)
+    sim.make_simulated_data()
+    sim.fit_simulated_data()
+    ipdb.set_trace()
